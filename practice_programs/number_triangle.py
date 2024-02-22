5b94b7ee351679167b5cd07b47730877598e6c9b
lagnesh712 <lagneshchouhan@gmail.com>
2024-02-22 17:41:17 +0530

    fixed commit

--- practice_programs/number_triangle.py
+++ practice_programs/number_triangle.py
@@ -1,5 +1,6 @@
 flag=0
-for i in range(1,9):
+max=6
+for i in range(1,max):
     for j in range(0,i):
         flag+=1
         print(flag,end=" ")
---
+++ ui_automation_basic/automation_brushup.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+from csv import DictWriter
+from pynput.keyboard import Key ,Controller
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+options=Options()
+options.add_experimental_option("prefs",{"download.default_directory":"/home/lagnesh/Desktop"})
+service_obj= Service("/home/lagnesh/interview_practice/utility/chromedriver")
+options.enable_downloads
+
+driver=webdriver.Chrome(service=service_obj,options=options)
+# Test Case 1:
+# Go to Google.com
+# Enter "IPL points table 2021" in search box
+# Capture all the Suggestions from the search box and store in List
+# Search for "ipl points table 2021" in list & Google it by Pressing Enter(Google Search) with use of Key Press
+driver.get("https://www.google.com")
+driver.find_element(By.XPATH,'//*[@id="APjFqb"]').send_keys("IPl points table 2021")
+wait=WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_all_elements_located((By.XPATH,'//*[@class="G43f7e"]/li')))
+option_list=driver.find_elements(By.XPATH,'//*[@class="G43f7e"]/li')
+suggestions=[]
+for i in option_list:
+    suggestions.append(i.text)
+print(suggestions)
+driver.find_element(By.XPATH,'//*[@id="APjFqb"]').send_keys(Keys.ENTER)
+# Test Case 2:
+# Go To https://www.iplt20.com/points-table/men/2021
+# From the Points Table Get Details of Team(Rank ,Team Name , Points) who has Points greater than 8.
+# Store the captured details in CSV file.
+driver.get("https://www.iplt20.com/points-table/men/2021")
+teams=driver.find_elements(By.XPATH,'//*[@id="pointsdata"]/tr')
+rank=0
+with open('./../data/data.csv','w') as f:
+    csv_writer=DictWriter(f,fieldnames=["Rank","Team Name", "Points"])
+    csv_writer.writeheader()
+    for i in teams:
+        points=int(i.find_element(By.XPATH,'td[11]').text)
+        if points>8:
+            rank=rank+1
+            csv_writer.writerow({
+                "Rank":rank,
+                "Team Name":(i.find_element(By.XPATH,'td[3]/div/h2').text),
+                "Points":points
+            })
+f.close()
+# Test Case 3:
+# Upload Test Case-2 Result CSV file on https://www.zamzar.com/convert/csv-to-pdf/
+# Convert into PDF using website engine.
+# Download and Save the PDF file on Desktop.
+driver.get("https://convertio.co/csv-pdf/")
+driver.find_element(By.XPATH,'//*[@class="file-source-button"]/label').click()
+keyboard= Controller()
+keyboard.type('/home/lagnesh/interview_practice/data/data.csv')
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+wait=WebDriverWait(driver,20)
+wait.until(expected_conditions.presence_of_element_located((By.XPATH,'//*[@class="btn btn-xl btn-primary"]')))
+driver.find_element(By.XPATH,'//*[@class="btn btn-xl btn-primary"]').click()
+wait.until(expected_conditions.visibility_of_element_located((By.XPATH,'//*[@class="btn btn-sm btn-blue"]')))
+driver.find_element(By.XPATH,'//*[@class="btn btn-sm btn-blue"]').click()
+
+
+
+
+
+    
